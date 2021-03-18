e91dc1225f5014503f4635c15bb693085ae1c754
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-03-18 22:43:11 +0200

    Added Board class
    This class controls board creation.

---
+++ Tic Tac Toe/Tic Tac Toe/Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    public class Board
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        private ObservableCollection<BoardCell> _cells;
+        public ObservableCollection<BoardCell> Cells
+        {
+            get
+            {
+                if (_cells == null)
+                    _cells = new ObservableCollection<BoardCell>(Enumerable.Range(0, Rows * Columns).Select(i => new BoardCell()));
+                return _cells;
+            }
+        }
+    }
+}
