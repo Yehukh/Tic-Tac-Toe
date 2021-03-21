62b168f6afa3fc72d5746e20cf5e8f66b5cc2d2b
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-03-21 22:19:21 +0200

    Created empty class for Win check

---
+++ Tic Tac Toe/Tic Tac Toe/CheckWinner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe;
+
+namespace Tic_Tac_Toe
+{
+    public class CheckWinner
+    {
+        public static string CheckForResult()
+        {
+            string result = "";
+            if (Board.Cells.Count < 9)
+            {
+
+            }
+
+
+            return result;
+        }
+    }
+}
