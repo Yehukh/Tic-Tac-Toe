500dd9676e6ec31721d3e16bd78b17674d236edd
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-03-29 18:57:07 +0300

    Added "Restart" button.

--- Tic Tac Toe/Tic Tac Toe/MainWindow.xaml.cs
+++ Tic Tac Toe/Tic Tac Toe/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using Tic_Tac_Toe.Pages;
+using Tic_Tac_Toe.Refresheres;
 
 namespace Tic_Tac_Toe
 {
@@ -35,6 +37,13 @@ namespace Tic_Tac_Toe
         private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
             Main.Content = new SettingsPage();
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            var cell = Board.Cells.FirstOrDefault(x => x.Sign != null);
+            if (cell != null) UpdateBoard.Update();
+            Main.Content = new BoardPage();
         }
     }
 }
