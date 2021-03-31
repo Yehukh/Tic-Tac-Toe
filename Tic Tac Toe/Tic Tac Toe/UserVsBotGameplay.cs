eda2a4fbfe942fe17c86faaa8ae4bddabc606d47
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-03-31 23:10:18 +0300

    Added mode indication

---
+++ Tic Tac Toe/Tic Tac Toe/UserVsBotGameplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    public static class UserVsBotGameplay
+    {
+        private static bool _userVsBot;
+        public static bool UserVsBot
+        {
+            get
+            {
+                return _userVsBot;
+            }
+            set
+            {
+                _userVsBot = value;
+            }
+        }
+    }
+}
