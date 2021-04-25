a4ed9957dec4c245d9704a648060c20b483cfd50
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-04-25 12:58:33 +0300

    Added logic for main view model

--- Tic Tac Toe/Tic Tac Toe/ViewModels/MainViewModel.cs
+++ Tic Tac Toe/Tic Tac Toe/ViewModels/MainViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Tic_Tac_Toe.Models;
 
 namespace Tic_Tac_Toe.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel
     {
+        private readonly BoardModel _board;
+
+        public BoardViewModel Board { get; set; }
+        public SettingsViewModel Settings { get; set; }
+
+        public MainViewModel()
+        {
+            _board = new BoardModel();
+
+            Board = new BoardViewModel(_board);
+            Settings = new SettingsViewModel(_board);
+        }
     }
 }
