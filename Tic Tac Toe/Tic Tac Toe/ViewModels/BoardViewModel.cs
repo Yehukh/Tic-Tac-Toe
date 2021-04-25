4e0f8660889edaa0b490862b665d81c81987898b
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-04-25 12:58:13 +0300

    Added logic for board view model

--- Tic Tac Toe/Tic Tac Toe/ViewModels/BoardViewModel.cs
+++ Tic Tac Toe/Tic Tac Toe/ViewModels/BoardViewModel.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Tic_Tac_Toe.Models;
 
 namespace Tic_Tac_Toe.ViewModels
 {
-    class BoardViewModel
+    public class BoardViewModel
     {
+        public static readonly RoutedCommand Command = new RoutedCommand();
+
+        public BoardModel Board { get; set; }
+
+        public ICommand Click { get; }
+
+        public BoardViewModel(BoardModel board)
+        {
+            Board = board;
+
+            Click = new RelayCommand(NotImplementedCommand);
+        }
+        private void NotImplementedCommand()
+        {
+
+        }
     }
 }
