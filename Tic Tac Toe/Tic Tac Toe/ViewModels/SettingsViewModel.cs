dadcc9242692e49e7b685506f52947df3fae91fa
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-04-25 12:58:04 +0300

    Added logic for settting view model

--- Tic Tac Toe/Tic Tac Toe/ViewModels/SettingsViewModel.cs
+++ Tic Tac Toe/Tic Tac Toe/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Windows.Input;
+using Tic_Tac_Toe.Models;
 
 namespace Tic_Tac_Toe.ViewModels
 {
-    class SettingsViewModel
+    public class SettingsViewModel
     {
+        public static readonly RoutedCommand Command = new RoutedCommand();
+        public BoardModel Board { get; set; }
+
+        public ICommand ChangeBoardSize { get; }
+        public ICommand ChangeGameMode { get; }
+        public ICommand ChangeBotDifficulty { get; }
+
+        public SettingsViewModel(BoardModel board)
+        {
+            Board = board;
+
+            ChangeBoardSize = new RelayCommand(NotImplementedCommand);
+            ChangeGameMode = new RelayCommand(NotImplementedCommand);
+            ChangeBotDifficulty = new RelayCommand(NotImplementedCommand);
+        }
+        private void NotImplementedCommand()
+        {
+
+        }
     }
 }
