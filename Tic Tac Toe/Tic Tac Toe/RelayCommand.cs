fa0969b612f212a0c313e0f0cf4238d929e182ca
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-04-25 12:27:50 +0300

    Implemented Relay Command

--- Tic Tac Toe/Tic Tac Toe/RelayCommand.cs
+++ Tic Tac Toe/Tic Tac Toe/RelayCommand.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Tic_Tac_Toe
 {
-    class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+        
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        
+        public void Execute(object parameter) => _execute(parameter);
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+        
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
     }
 }
