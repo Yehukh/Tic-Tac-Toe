8fb8b1020c33c9a5c4746d9e5de6c0083ea90508
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-04-28 20:26:54 +0300

    Implemented Caching engine

--- Tic Tac Toe/Tic Tac Toe.Engine/Engines/CachingEngine.cs
+++ Tic Tac Toe/Tic Tac Toe.Engine/Engines/CachingEngine.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Tic_Tac_Toe.Engine.Interfaces;
 
 namespace Tic_Tac_Toe.Engine.Engines
 {
-    class CachingEngine
+    public class CachingEngine : IEngine
     {
+        private static readonly Dictionary<int, int> _moveCache = new Dictionary<int, int>();
+
+        private readonly IEngine _engine;
+ 
+        public CachingEngine(IEngine engine)
+            => _engine = engine ?? throw new ArgumentNullException(nameof(engine));
+        
+        public int FindBestMove(Board board)
+        {
+            int key = board.GetKey();
+
+            if (!_moveCache.TryGetValue(key, out int move))
+            {
+                move = _engine.FindBestMove(board);
+                _moveCache.Add(key, move);
+            }
+
+            return move;
+        }
     }
 }
