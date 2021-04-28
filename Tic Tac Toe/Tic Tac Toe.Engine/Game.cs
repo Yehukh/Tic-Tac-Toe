c1d095508a0e69a4185b0bc79f42bfa378550afe
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-04-28 20:27:53 +0300

    Implemented Game class

--- Tic Tac Toe/Tic Tac Toe.Engine/Game.cs
+++ Tic Tac Toe/Tic Tac Toe.Engine/Game.cs
@@ -1,10 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Tic_Tac_Toe.Engine.Engines;
+using Tic_Tac_Toe.Engine.Interfaces;
 
 namespace Tic_Tac_Toe.Engine
 {
-    class Game
-    {
-    }
+	public class Game : IGame
+	{
+		private Board _board = new Board();
+		private readonly IEngine _engine;
+		
+		public Game() : this(new CachingEngine(new AlphaBetaPruningEngine())) { }
+		public Game(IEngine engine) => _engine = engine ?? throw new ArgumentNullException(nameof(engine));
+		
+		public ref Board Board => ref _board;
+		public Winner Winner { get; internal set; } = Winner.None;
+		
+		public MoveResult MakeMove(int fieldIdx)
+		{
+			if (!_board.IsMoveLegal(fieldIdx)) return MoveResult.IllegalMove;
+
+			return this.PerformMove(fieldIdx, FieldState.User);
+		}
+		
+		public MoveResult MakeMachineMove()
+		{
+			int bestMove = _engine.FindBestMove(_board);
+
+			if (bestMove > -1)
+				return this.PerformMove(bestMove, FieldState.Machine);
+			else
+				return new MoveResult(false, true);
+		}
+		
+		private MoveResult PerformMove(int fieldIdx, FieldState fieldState)
+		{
+			_board[fieldIdx] = fieldState;
+			this.CheckWinner();
+
+			return new MoveResult(true, this.IsFinal());
+		}
+		
+		internal void CheckWinner() => this.Winner = _board.CheckWinner();
+		
+		internal bool IsFinal()
+		{
+			if (this.Winner == Winner.None)
+			{
+				if (_board.HasEmptyFields())
+					return false;
+
+				this.Winner = Winner.Draw;
+			}
+
+			return true;
+		}
+	}
 }
