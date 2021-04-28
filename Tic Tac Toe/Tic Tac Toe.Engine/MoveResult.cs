dd4522e1288d7077260abc8d9fe7b0889410c750
Yehor Kukhar <yehor.kukhar@gmail.com>
2021-04-28 20:24:12 +0300

    Implemented Move Result class

--- Tic Tac Toe/Tic Tac Toe.Engine/MoveResult.cs
+++ Tic Tac Toe/Tic Tac Toe.Engine/MoveResult.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Tic_Tac_Toe.Engine
+﻿namespace Tic_Tac_Toe.Engine
 {
-    class MoveResult
+    public readonly struct MoveResult
     {
+        private static readonly MoveResult s_illegalMove = new MoveResult(false, false);
+        
+        public static ref readonly MoveResult IllegalMove => ref s_illegalMove;
+
+        public bool MoveIsLegal { get; }
+        public bool GameEnded { get; }
+        
+        public MoveResult(bool moveIsLegal, bool gameEnded)
+        {
+            this.MoveIsLegal = moveIsLegal;
+            this.GameEnded = gameEnded;
+        }
     }
 }
